2c9a739d0cff3c79624d6dd751726c1aba6e838c
Aman Thapliyal <kira@Amans-MacBook-Air.local>
2024-12-25 12:36:31 +0530

    Weather Application

---
+++ Weather App/images/first.js
@@ -0,0 +1,47 @@
+const apiKey="ff5ddf071599b8209a771972898a17ae";
+const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchbox=document.querySelector(`.search input`);
+const searchbtn=document.querySelector(`.search button`);
+const weatherIcon=document.querySelector(`.weather-icon`);
+
+async function checkweather(city){
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+
+
+    if(response.status==404){
+        document.querySelector(`.error`).style.display=`block`;
+        document.querySelector(`.weather`).style.display=`none`;
+
+    }else{
+        var data=await response.json();
+
+    
+        document.querySelector(`.city`).innerHTML=data.name;
+        document.querySelector(`.temp`).innerHTML=Math.round(data.main.temp)  + `&degc`;
+        document.querySelector(`.humidity`).innerHTML=data.main.humidity + `%`;
+        document.querySelector(`.wind`).innerHTML=data.wind.speed + `km/hr`;
+    
+        if(data.weather[0].main==`Clouds`){
+            weatherIcon.src=`clouds.png`;
+        }else if(data.weather[0].main==`Clear`){
+            weatherIcon.src=`clear.png`;
+        }else if(data.weather[0].main==`Rain`){
+            weatherIcon.src=`rain.png`;
+        }else if(data.weather[0].main==`Drizzle`){
+            weatherIcon.src=`drizzle.png`;
+        }else if(data.weather[0].main==`Mist`){
+            weatherIcon.src=`mist.png`;
+        }
+    
+        document.querySelector(`.weather`).style.display=`block`;
+        document.querySelector(`.error`).style.display=`none`;
+
+    }
+}
+
+searchbtn.addEventListener(`click`,()=>{
+    checkweather(searchbox.value);
+})
+
+
