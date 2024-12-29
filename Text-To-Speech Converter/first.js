5cbc3e28026fb42986d606624655e8023993e5bc
Aman Thapliyal <kira@Amans-MacBook-Air.local>
2024-12-29 12:51:05 +0530

    Text to speech converter

---
+++ Text-To-Speech Converter/first.js
@@ -0,0 +1,27 @@
+let speech=new SpeechSynthesisUtterance();
+
+let voices=[];
+
+let voiceSelect=document.querySelector(`select`);
+
+window.speechSynthesis.onvoiceschanged=()=>{
+    voices=window.speechSynthesis.getVoices();
+    speech.voice=voices[0];
+
+    voices.forEach((voice, i) => {
+        let option = new Option(voice.name, i);
+        voiceSelect.add(option);
+    });
+};
+
+voiceSelect.addEventListener(`change`,()=>{
+    speech.voice=voices[voiceSelect.value];
+});
+
+const button=document.querySelector(`button`);
+
+button.addEventListener(`click`,()=>{
+    speech.text=document.querySelector(`textarea`).value;
+    window.speechSynthesis.speak(speech);
+})
+
